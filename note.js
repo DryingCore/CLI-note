a6894f876e1785f7f538a9b51769df27874ecf28
Lein0rb4gX <lein0rb4g@gmail.com>
2024-01-19 14:22:56 -0300

    Changes to be committed: 	modified:   note.js 	modified:   package-lock.json

--- note.js
+++ note.js
@@ -1,31 +1,52 @@
+// Importando bibliotecas necessárias
 const fs = require("fs-extra");
 const readline = require("readline");
 const path = require("path");
+
+// Criando a interação com o usuário
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 });
 
+// Funções para dividir tarefas
+function obterTitulo(callback) {
+	rl.question("Digite o título: ", callback);
+}
+
+function obterAnotacao(callback) {
+	rl.question("Digite sua anotação: ", callback);
+}
+
+function obterCaminhoPasta(callback) {
+	rl.question("Digite um caminho (pressione Enter para usar o padrão): ", callback);
+}
+
+// Função que cria a nota
 function criarNota() {
-	rl.question("Digite o título: ", (titulo) => {
-		rl.question("Digite sua anotação: ", (nota) => {
-			rl.question("Digite um caminho (pressione Enter para usar o padrão): ", (caminho_pasta) => {
+	obterTitulo((titulo) => {
+		obterAnotacao((nota) => {
+			obterCaminhoPasta((caminho_pasta) => {
 				const pasta = caminho_pasta || "C:/Users/lein0/OneDrive/Documentos/Anotações";
-				// Gerar um arquivo com o título passado e na pasta de destino definida.
 				const nome_arquivo = path.join(pasta, `${titulo}.txt`);
 
-				// Escrever nota no arquivo
-				fs.writeFile(nome_arquivo, nota, (error) => {
-					if (error) {
-						console.error(error);
-					} else {
-						console.log(`Anotação salva: ${nome_arquivo}`);
-						rl.close();
-					}
-				});
+				escreverArquivo(nome_arquivo, nota, titulo);
 			});
 		});
 	});
 }
 
+// Função que cria o arquivo com os dados passados
+function escreverArquivo(nome_arquivo, nota, titulo) {
+	fs.writeFile(nome_arquivo, nota, (error) => {
+		if (error) {
+			console.error(error);
+		} else {
+			console.log(`Anotação "${titulo}" salva.`);
+			rl.close();
+		}
+	});
+}
+
+// Chamada da função
 criarNota();
